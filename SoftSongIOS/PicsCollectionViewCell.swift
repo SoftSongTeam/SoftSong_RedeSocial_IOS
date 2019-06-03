3d8a16e9a52de0d8e60694486b3ea51bbfbbadf1
ferrella <felipe.perrella2210@gmail.com>
2019-06-03 20:46:12 -0300

    Midia dos Posts totalmente operantes

---
+++ SoftSongIOS/PicsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PicsCollectionViewCell.swift
+//  SoftSongIOS
+//
+//  Created by Felipe  Perrella on 25/05/19.
+//  Copyright © 2019 Felipe Perrella. All rights reserved.
+//
+
+import UIKit
+
+class PicsCollectionViewCell: UICollectionViewCell {
+    
+}
